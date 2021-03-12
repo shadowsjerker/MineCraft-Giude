faf20e1494949e0dfc15b585415ec128c8c9f976
ShadowsJerker <75665031+shadowsjerker@users.noreply.github.com>
2021-03-12 10:51:49 +0330

    Update HomePage.xaml.cs

--- MineCraft Guide/MineCraft Guide/MineCraft Guide/HomePage.xaml.cs
+++ MineCraft Guide/MineCraft Guide/MineCraft Guide/HomePage.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Windows.UI.ViewManagement;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,7 +15,12 @@ namespace MineCraft_Guide
         public HomePage()
         {
             InitializeComponent();
+            
         }
+
+        
+
+       
 
         private void Button_Clicked(object sender, EventArgs e)
         {
